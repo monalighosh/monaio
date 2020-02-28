bbab2ddbdc6e5d20277519799f5f5fe3ae44b518
Monali Ghosh <monalighosh@users.noreply.github.com>
2020-02-28 20:19:56 +1100

    Implemented firebase utils, included ability to autheticate users, add users in database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from './pages/Homepage/Homepage';
 import Shoppage from './pages/Shoppage/Shoppage';
 import Header from './components/Header/Header'
 import SignInSignUpPage from './pages/SignInSignUpPage/SignInSignUpPage';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -20,8 +20,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({ currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+            } 
+          });
+        })
+      }
+     this.setState({currentUser: userAuth})
     });
   }
 
--- src/pages/SignInSignUpPage/SignInSignUpPage.js
+++ src/pages/SignInSignUpPage/SignInSignUpPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './SignInSignUpPage.scss';
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 class SignInSignUpPage extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className='sign-in-sign-up'>
         <SignIn />
+        <SignUp />
       </div>
     );
   }
